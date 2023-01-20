55bba0b15ac8e0975b7e06a0f7629087ef081ae7
Jigah Miracle <jigah4july@gmail.com>
2023-01-20 09:17:05 +0100

    Logout

--- frontend/src/feature/auth/auth.service.js
+++ frontend/src/feature/auth/auth.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const API_URL = process.env.NODE_ENV === "development" ? 'http://localhost:4000/api/users/' : `https://loan-backend.onrender.com/api/users/`;
-const API_URL = 'http://localhost:4000/api/users/';
+// const API_URL = 'http://localhost:4000/api/users/';
 
 const register = async (userData) => {
     const response = await axios.post(`${API_URL}signup`, userData)
