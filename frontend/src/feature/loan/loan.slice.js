abc91096311061e65b7e539606492b7961e4b224
Jigah Miracle <jigah4july@gmail.com>
2023-02-20 12:07:35 +0100

    Dashboard Features Updates!

--- frontend/src/feature/loan/loan.slice.js
+++ frontend/src/feature/loan/loan.slice.js
@@ -6,7 +6,7 @@ const loan = JSON.parse(localStorage.getItem("loan"));
 const initialState = {
     loan: loan ? loan : null,
     isError: false,
-    isSuccess: true,
+    isSuccess: false,
     isLoading: false,
     message: "",
 }
